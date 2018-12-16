9332f6f7d577f79238e40e2f7faa0f325b3589b6
Karthik Vinnakota <vvvk28@gmail.com>
2018-12-16 14:10:51 -0800

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Karthik Vinnakota on 12/16/18.
+//  Copyright © 2018 Karthik Vinnakota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
